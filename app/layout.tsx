895912012f18bba7ac29e8359a963aeba2cc1a40
echozyr2001 <echo.zyr.2001@gmail.com>
2025-01-04 11:03:35 +0800

    init commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="min-h-full m-8">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
