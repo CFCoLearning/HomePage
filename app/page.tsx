8d42d0690e47ea04685ccee6835ef0189102008b
echozyr2001 <echo.zyr.2001@gmail.com>
2025-01-04 17:22:19 +0800

    disable eslint

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { Hero } from "@/components/hero/hero";
 import { Projects } from "@/components/projects/projects";
-import Image from "next/image";
 
 export default function Home() {
   return (
