13a898839f34b3fbb87905dbd879b8071f833399
echozyr2001 <echo.zyr.2001@gmail.com>
2025-01-13 22:36:06 +0800

    enter animation

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -1,14 +1,23 @@
 "use client";
+
+import { motion } from "motion/react";
 import React from "react";
 import { Boxes } from "@/components/ui/background-boxes";
 import CTA from "./cta";
 
 export function Hero() {
   return (
-    <div className="h-96 relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
+    <motion.div
+      className="h-96 relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg"
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        scale: { type: "spring", visualDuration: 0.9, bounce: 0.1 },
+      }}
+    >
       <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
       <Boxes />
       <CTA />
-    </div>
+    </motion.div>
   );
 }
