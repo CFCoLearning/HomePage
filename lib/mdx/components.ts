0ac86187b6054c0866019b93993c01b3e89d76a2
echozyr2001 <echo.zyr.2001@gmail.com>
2025-01-14 22:51:44 +0800

    project header

--- lib/mdx/components.ts
+++ lib/mdx/components.ts
@@ -1,7 +1,9 @@
 import Image from "next/image";
 import { ProgressTable } from "@/components/mdx/projects/progress-table";
+import { ProjectHeader } from "@/components/mdx/projects/project-header";
 
 export const components = {
   ProgressTable,
+  ProjectHeader,
   Image,
 };
