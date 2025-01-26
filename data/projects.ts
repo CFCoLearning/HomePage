1d0354228bf805447b2e3ef7a033914f876c48e1
echozyr2001 <echo.zyr.2001@gmail.com>
2025-01-26 18:55:05 +0800

    project info data

--- data/projects.ts
+++ data/projects.ts
@@ -3,10 +3,18 @@ import { ProjectInfo } from "@/lib/project";
 export const ProjectInfos: ProjectInfo[] = [
   {
     title: "The-Missing-Semester",
-    pageUrl: "https://www.google.com",
+    pageUrl: "/projects/The-Missing-Semester",
     githubUrl: "https://www.github.com/CFCoLearning/The-Missing-Semester",
     startDate: "2025-01-06",
     endDate: "2025-01-26",
-    tags: ["git", "vim", "数据处理"],
+    tags: ["计算工具", "命令行", "版本控制", "自动化", "现代工作流"],
+  },
+  {
+    title: "FullStack-Compass",
+    pageUrl: "/projects/FullStack-Compass",
+    githubUrl: "https://www.github.com/CFCoLearning/FullStack-Compass",
+    startDate: "2025-02-03",
+    endDate: "2025-02-23",
+    tags: ["前端", "SaaS", "产品设计"],
   },
 ];
