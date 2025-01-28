5a04b1211b616d797ef21a0c3606d0965da46676
echozyr2001 <echo.zyr.2001@gmail.com>
2025-01-28 16:12:45 +0800

    docs page

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -3,6 +3,7 @@ import { notFound } from "next/navigation";
 import { getDocument } from "@/lib/markdown";
 import { PageBreadcrumb } from "@/components/docs/pagebreadcrumb";
 import { Toc } from "@/components/docs/toc";
+import { Typography } from "@/components/typography";
 
 type PageProps = {
   params: Promise<{ slug: string[] }>;
@@ -21,14 +22,15 @@ export default async function Pages({ params }: PageProps) {
     <div className="flex items-start gap-14">
       <div className="flex-[3] pt-10">
         <PageBreadcrumb paths={slug} />
-        <div className="typography">
-          <h1 className="text-3xl -mt-2">{frontmatter.title}</h1>
-          <p className="-mt-4 text-base text-muted-foreground text-[16.5px]">
-            {frontmatter.description}
-          </p>
-          <div>{content}</div>
-          {/* <Pagination pathname={pathName} /> */}
-        </div>
+        <Typography>
+          <div className="typography">
+            <h1 className="text-3xl -mt-2">{frontmatter.title}</h1>
+            <p className="-mt-4 text-base text-muted-foreground text-[16.5px]">
+              {frontmatter.description}
+            </p>
+            <div>{content}</div>
+          </div>
+        </Typography>
       </div>
       <div className="hidden xl:flex xl:flex-col sticky top-16 gap-3 py-8 min-w-[230px] h-[94.5vh] toc">
         <Toc tocs={tocs} />
--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex items-start gap-14">
+    <div className="flex items-start gap-14 m-10">
       <Sidebar />
       <div className="flex-1 md:flex-[6]">{children}</div>
     </div>
