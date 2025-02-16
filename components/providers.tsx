931a6baad766edeb7bf73f4ea724604da825f572
echozyr2001 <echo.zyr.2001@gmail.com>
2025-02-16 21:09:04 +0800

    solana wallet

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,9 +1,14 @@
 import { ConvexClientProvider } from "./convex-client-provider";
+import Web3WalletProvider from "./web3-wallet-provider";
 
 export function Providers({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <ConvexClientProvider>{children}</ConvexClientProvider>;
+  return (
+    <ConvexClientProvider>
+      <Web3WalletProvider>{children}</Web3WalletProvider>
+    </ConvexClientProvider>
+  );
 }
