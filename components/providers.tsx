9c5517b2802c2edb1ffc7f786de52ae09c501b16
echozyr2001 <echo.zyr.2001@gmail.com>
2025-02-18 23:40:43 +0800

    unuse solana use reown

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,14 +1,9 @@
 import { ConvexClientProvider } from "./convex-client-provider";
-import Web3WalletProvider from "./web3-wallet-provider";
 
 export function Providers({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <ConvexClientProvider>
-      <Web3WalletProvider>{children}</Web3WalletProvider>
-    </ConvexClientProvider>
-  );
+  return <ConvexClientProvider>{children}</ConvexClientProvider>;
 }
