127a96ad54af88488cd42f5cd814aff7c8f03fbe
echozyr2001 <echo.zyr.2001@gmail.com>
2025-02-19 16:10:06 +0800

    connect before register

--- app/hackathon/(components)/register-button.tsx
+++ app/hackathon/(components)/register-button.tsx
@@ -10,6 +10,9 @@ import {
   DialogDescription,
 } from "@/components/ui/dialog";
 import { RegisterForm } from "./register-form";
+import { useAppKit, useAppKitAccount } from "@reown/appkit/react";
+import { toast } from "@/hooks/use-toast";
+import { Button } from "@/components/ui/button";
 
 interface RegisterButtonProps {
   isRegistrationEnded: boolean;
@@ -20,7 +23,39 @@ export function RegisterButton({
   isRegistrationEnded,
   eventName,
 }: RegisterButtonProps) {
+  const { isConnected } = useAppKitAccount();
+  const { open } = useAppKit();
   const [isOpen, setIsOpen] = useState(false);
+
+  const handleTriggerClick = (e: React.MouseEvent) => {
+    if (!isConnected) {
+      e.preventDefault();
+      toast({
+        title: "Wallet Not Connected",
+        description: (
+          <div className="mt-2 rounded-md bg-red-950 p-4">
+            <p className="text-red-200">
+              请请先连接你的钱包，然后再进行报名操作。
+            </p>
+          </div>
+        ),
+        action: (
+          <div className="flex items-center justify-center mt-4 h-full">
+            <Button
+              onClick={() => {
+                open();
+              }}
+              className="px-6 py-3 bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 rounded-lg text-sm text-white transition-all duration-200 transform hover:scale-105"
+            >
+              Connect Now
+            </Button>
+          </div>
+        ),
+      });
+      return;
+    }
+    setIsOpen(true);
+  };
 
   const handleClose = () => {
     setIsOpen(false);
@@ -31,6 +66,7 @@ export function RegisterButton({
       <DialogTrigger
         className="block w-full py-3 px-4 bg-green-500 hover:bg-green-600 text-white font-bold rounded-lg text-center transition-colors duration-300 disabled:opacity-50 disabled:cursor-not-allowed"
         disabled={isRegistrationEnded}
+        onClick={handleTriggerClick}
       >
         {isRegistrationEnded ? "Registration Deadline" : "Register Now"}
       </DialogTrigger>
